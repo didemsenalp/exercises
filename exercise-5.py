f4ab75d392a26d44133338d73b7b89be59f744db
Didem Şenalp <didemsenalp8@gmail.com>
2023-04-17 15:42:26 +0300

    exercise-5 done

--- exercise-5.py
+++ exercise-5.py
@@ -1,4 +1,16 @@
-def test():
-    x = 5
-    y = 6
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
+print(numbers_x[0])
+print(numbers_x[-1])
+if (numbers_x[0] == numbers_x[-1]):
+    print("result is True")
+else:
+    print("result is False")
 
+
+print(numbers_y[0])
+print(numbers_y[-1])
+if (numbers_y[0] == numbers_y[-1]):
+    print("result is True")
+else:
+    print("result is False")
