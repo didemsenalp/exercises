65208c6f173e73dc4c74286fefd03c3ca1a35acd
Didem Şenalp <didemsenalp8@gmail.com>
2023-04-17 11:28:42 +0300

    exercise 1,2 done

---
+++ exercise-1.py
@@ -0,0 +1,11 @@
+num1 = int(input("1.sayi:"))
+num2 = int(input("2.sayi:"))
+
+mult = int(num1 * num2)
+sum = int(num1 + num2)
+
+if mult <= 1000:
+    print("The result is:", mult)
+    
+else:
+    print("The result is:", sum)
---
+++ exercise-2.py
@@ -0,0 +1,9 @@
+
+
+
+for a in range (10):
+    b = a + 1
+    sum = a + b
+    print("Previous Number:", a," Current Number:", b," Sum: ",sum)
+
+     
