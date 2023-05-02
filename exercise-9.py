b4c4e2aa68c778011fbad33a1bddcb8fef4f07ff
Didem Şenalp <didemsenalp8@gmail.com>
2023-05-02 16:11:07 +0300

    9 done

--- exercise-9.py
+++ exercise-9.py
@@ -1,10 +1,19 @@
-num = int(input("Lütfen bir sayı giriniz:"))
-reversed_num = 0
 
-while num != 0:
-    digit = num % 10
-    reversed_num = reversed_num * 10 + digit
-    num //= 10
+def isPalindrome(num):
 
-print("Reversed Number: " + str(reversed_num))
+    numbers = num
+
+    reversed_num = 0
+
+    while num != 0:
+        digit = num % 10
+        reversed_num = reversed_num * 10 + digit
+        num //= 10
+
+    if numbers == reversed_num:
+        return True
+    else:
+        return False
+
+print(isPalindrome(num = int(input("Lütfen bir sayı giriniz:"))))
 
